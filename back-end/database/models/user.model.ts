62c6872f829c0e1c746342a968e74b2dbbc2157c
Mohammad Farah <mhd.frh.bus@gmail.com>
2024-08-01 14:00:37 +0300

    creating user interface and model for mongoose orm at interfaces/user.interface.ts and models/user.model.ts

---
+++ back-end/database/models/user.model.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from 'mongoose';
+import { IUser } from './interfaces/user.interface';
+
+const UserSchema: Schema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: 'user' },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IUser>('User', UserSchema);
